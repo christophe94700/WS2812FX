6dd0aa0460397dcdd1280714cd76fe92c6f84d29
Christophe CARON <best@caron.tv>
2018-08-17 20:24:17 +0200

    Update Fonction_EEPROM.ino

--- esp8266_web_Alexa/Fonction_EEPROM.ino
+++ esp8266_web_Alexa/Fonction_EEPROM.ino
@@ -66,7 +66,7 @@ String LectureStringEeprom(int adresse, int taille)
   String valeur = "";
   for (int i = 0; i < taille; ++i)
   {
-    if (((EEPROM.read(i + adresse))<32)||((EEPROM.read(i + adresse))>122)) return (WiFi.hostname().c_str()); // Caractères entre 32 et 122 retour nom du module
+    if ((EEPROM.read(i + adresse))==0) return (WiFi.hostname().c_str()); // Caractères entre 32 et 122 retour nom du module
     valeur += char(EEPROM.read(i + adresse));
   }
   return String(valeur);
